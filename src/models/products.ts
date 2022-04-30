d74f6ff632d7fe68570764218f34f9852937c831
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-04-30 23:10:39 +0100

    added crud functionality for products table

---
+++ src/models/products.ts
@@ -0,0 +1,47 @@
+import { type } from "os";
+import client from "../database";
+
+export type Product = {
+    name : string,
+    price: string,
+    category: string
+};
+
+export class ProductStore {
+  async index(): Promise<Product[]>{
+      try {
+          const conn = await client.connect();
+          const sql = 'SELECT * FROM products RETURNING *';
+          const result = await conn.query(sql);
+          conn.release();
+          return result.rows
+      } catch (error) {
+         throw new Error(`could not get products ${error}`) 
+      }
+  }
+
+  async show(id: string): Promise<Product>{
+      try {
+          const conn = await client.connect();
+          const sql = 'SELECT * FROM products WHERE id = ($1)';
+          const result = await conn.query(sql, [id]);
+          conn.release();
+          return result.rows[0];
+      } catch (error) {
+          throw new Error(`could not get products with id: ${id}`)
+      }
+  }
+
+  async create(product: Product): Promise<Product>{
+      try {
+          const conn = await client.connect();
+          const sql = 'INSERT INTO products (name, price, category) VALUES ($1, $2, $3) RETURNING *';
+          const values = [product.name, product.price, product.category];
+          const result = await conn.query(sql, values);
+          conn.release();
+          return result.rows[0]
+      } catch (error) {
+          throw new Error(`could not create new product ${error}`)
+      }
+  }
+}
