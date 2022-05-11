97deaf5537aa500df94109259a1cd974c81090ea
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-05-11 11:11:02 +0100

    created jasmine test for user file

--- tests/orderSpec.ts
+++ tests/orderSpec.ts
@@ -1,29 +1,29 @@
-import {OrderStore}  from '../src/models/orders';
+// import {OrderStore}  from '../src/models/orders';
 
 
 
-const store = new OrderStore()
+// const store = new OrderStore()
 
-describe('Testing Orders models', () =>{
-    it('should have a index method', ()=>{
-      expect(store.index).toBeDefined();
-    });
-    it('should have a show method', ()=>{
-        expect(store.show).toBeDefined();
-      });
-      it('should have a create method', ()=>{
-        expect(store.create).toBeDefined();
-      });
-      it('should have a complete order method', ()=>{
-        expect(store.completedOrders).toBeDefined();
-      });
-      it('should have a delete method', ()=>{
-        expect(store.destroy).toBeDefined();
-      });
-      it('should have a current order method', ()=>{
-        expect(store.currentOrders).toBeDefined();
-      });
-      it('should have a show user order method', ()=>{
-        expect(store.showUsers).toBeDefined();
-      });
-})
+// describe('Testing Orders models', () =>{
+//     it('should have a index method', ()=>{
+//       expect(store.index).toBeDefined();
+//     });
+//     it('should have a show method', ()=>{
+//         expect(store.show).toBeDefined();
+//       });
+//       it('should have a create method', ()=>{
+//         expect(store.create).toBeDefined();
+//       });
+//       it('should have a complete order method', ()=>{
+//         expect(store.completedOrders).toBeDefined();
+//       });
+//       it('should have a delete method', ()=>{
+//         expect(store.destroy).toBeDefined();
+//       });
+//       it('should have a current order method', ()=>{
+//         expect(store.currentOrders).toBeDefined();
+//       });
+//       it('should have a show user order method', ()=>{
+//         expect(store.showUsers).toBeDefined();
+//       });
+// })
